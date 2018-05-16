15af21be9684607bf21242903860c0fc89e7d37a
Vladimir Sarychev <svmyhome@mail.ru>
2018-05-16 22:37:53 +0300

    Initialization progect

---
+++ Example2.java
@@ -0,0 +1,11 @@
+class Example2{
+	public static void main(String[] args) {
+		int var1;
+		int var2;
+		var1 = 1024;
+		System.out.println("Equal in var1 = " + var1);
+		var2 = var1 /2;
+		System.out.print("Equal in var2 = ");
+		System.out.println(var2);
+	}
+}
---
+++ Example3.java
@@ -0,0 +1,12 @@
+class Example3{
+	public static void main(String[] args) {
+		int var;
+		double x;
+
+		var = 10;
+		x = 10.0;
+		var = var/4;
+		x = x /4;
+		System.out.println(x +" : " + var);
+	}
+}
