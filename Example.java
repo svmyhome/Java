ddbabf864119852b30e918efb799ac489f606444
Vladimir Sarychev <svmyhome@mail.ru>
2018-05-21 18:51:04 +0300

    fatal err

--- Example.java
+++ Example.java
@@ -1,10 +1,4 @@
 ﻿ss Example{
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("JAVA rools from WORLD");
-=======
-		System.out.println("JAVA rools from WORLD!!!");
-		System.out.println("and Java Best language!!!");
->>>>>>> testing
-	}
 }
